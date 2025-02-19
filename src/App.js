bb8d378314f3bf5ff5b393751e445c16a9e53879
Your Name <you@example.com>
2025-02-18 20:58:06 -0400

    correção

--- src/App.js
+++ src/App.js
@@ -1,67 +1,62 @@
 import './App.css';
-import { Fragment , useState } from 'react';
+import { Fragment, useState } from 'react';
 import { fetchAlunos, createAluno, updateAluno, deleteAluno } from './service/http'
 
 function App() {
-  
-  const [aluno , setAluno] = useState({id: '',nome: '', curso: ''});
-  const [alunos , setAlunos] = useState([]);
+
+  const [aluno, setAluno] = useState({ id: '', nome: '', curso: '' });
+  const [alunos, setAlunos] = useState([]);
 
   async function loadAlunos() {
     const alunos = await fetchAlunos();
+    console.log(alunos)
     setAlunos(alunos);
   }
-
-  async function createAlunoApi(aluno) {
+  async function createAlunoAPI(aluno) {
     await createAluno(aluno);
-    setAluno({id: '', nome: '', curso: ''});
+    setAluno({ id: '', nome: '', curso: '' });
     loadAlunos();
-    
   }
   async function updateAlunoAPI(aluno) {
     await updateAluno(aluno);
-    setAluno({id: '', nome: '', curso: ''});
+    setAluno({ id: '', nome: '', curso: '' });
     loadAlunos();
-    
   }
-  async function deleteAlunoAPI(aluno) {
-    await deleteAluno(aluno);
+  async function deleteAlunoAPI(id) {
+    await deleteAluno(id);
     loadAlunos();
-    
   }
   return (
     <Fragment>
       <div className="container">
         <h1>Gerenciamento de Alunos</h1>
-
         <form id="alunoForm" onSubmit={(event) => {
           event.preventDefault();
-          createAlunoApi(aluno);
+          createAlunoAPI(aluno);
         }}>
-          <input type="hidden" id="alunoId" onChange={(value) =>
-            setAluno({...aluno, id: value.target.value})
-          } value={aluno.id} />
-          <input type="text" id="nome" placeholder="Nome" onChange={(value) =>
-            setAluno({...aluno, nome: value.target.value})
-          } required value={aluno.nome}/>
-          <input type="text" id="curso" placeholder="Curso" onChange={(value) =>
-            setAluno({...aluno, curso: value.target.value})
-          } required value={aluno.curso}/>
-          <button onClick={() => createAlunoApi(aluno)}>Salvar</button>
+            <input type="hidden" id="alunoId" onChange={(value) => { 
+              setAluno({...aluno, id: value.target.value})
+             }} value={aluno.id} />
+            <input type="text" id="nome" placeholder="Nome" onChange={(value) => {
+              setAluno({...aluno, nome: value.target.value})
+            }} required value={aluno.nome} />
+            <input type="text" id="curso" placeholder="Curso" onChange={(value) => {
+              setAluno({...aluno, curso: value.target.value})
+            }} required value={aluno.curso} />
+            <button type='submit'>Salvar</button>
         </form>
         <h2>Lista de Alunos</h2>
         <ul id="alunosList">
-          {alunos.map(aluno => {
-            <li key={aluno.id}>
+          { (alunos.map(aluno => {
+            return <li key={aluno.id}>
               {aluno.nome} - {aluno.curso}
               <button onClick={() => updateAlunoAPI(aluno)}>Editar</button>
-              <button onClick={() => deleteAlunoAPI(aluno)}>Deletar</button>
+              <button onClick={() => deleteAlunoAPI(aluno.id)}>Deletar</button>
             </li>
-          })}
+          }))}
         </ul>
       </div>
     </Fragment>
   );
 }
-
 export default App;
